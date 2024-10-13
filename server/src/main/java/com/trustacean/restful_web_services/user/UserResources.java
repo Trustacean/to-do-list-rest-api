619eb28068afec2f6dc42a7636a4451b21a7f88d
trustacean <pizzacrustacean15@gmail.com>
2024-10-13 19:17:36 +0700

    fix: added request mapping path "/api/v1" for user service

--- server/src/main/java/com/trustacean/restful_web_services/user/UserResources.java
+++ server/src/main/java/com/trustacean/restful_web_services/user/UserResources.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserResources {
     private UserService service;
 
